95d30b97d020615f663106838b6d60353cdb6774
natanri <riosjhonatan44@gmail.com>
2022-05-11 21:53:52 -0500

    I changed some methods and commented it

--- hilo.py
+++ hilo.py
@@ -1,19 +1,61 @@
-from pyclbr import Class
 import random
-Points = 300
-# TODO: Implement the Die class as follows...
+
+POINTS = 300
 class pick_a_card:
+    """
+    this class will show us the random number for the first card
+    """
+    
     def __init__(self):
         self.value = 0
-        self.point = Points
-    def Player(self):
-        print(self.point)
+        self.points = POINTS
+        
+    """
+    This method will pull the first card
+    
+    """
+    def display_card(self):
         self.value = random.randint(1, 13)
-        print(f"the card is {self.value}")
-class game:
-    def __init__(self):
-        self.guess = input("Higher or lower? [h/l] ")
-    def guess_card(self, Player):
-        print()
-        if self.guess == "l":
-           print(pick_a_card.Player.value(self))
+        print(f"The card is: {self.value}")
+        
+    """
+    This will display the text and ask for letter if the next number is lower or higher
+    """
+    def input_letter(self):
+        (input("Higher or lower? [h/l] "))
+        
+        
+    """
+    Then display the next number
+    """
+    def display_next_card(self):
+        self.value = random.randint(1, 13)
+        print(f"Next card was: {self.value}")
+    
+class Game(pick_a_card):
+    """
+    This method suppose to get the value and added or subtract depend on the letter, we are still working on this method.
+    """
+    
+    def __init__(self, value, points):
+        super().__init__(value, points)
+        
+    def score(self):
+        if self.display_next_card > self.display_card:
+            self.points = self.points + 100
+        else:
+            return self.points - 75
+            
+            
+        
+   
+a = pick_a_card()
+a.display_card()
+a.input_letter()
+a.display_next_card()
+b = Game()
+b.score()
+    
+    
+    
+
